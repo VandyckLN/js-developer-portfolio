75805ee8cffe745e02ea6927264b0138fa334872
VandyckLN <vandycklima@gmail.com>
2025-03-14 15:01:08 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,8 @@
 
 async function fetchProfileData() {
-const url = 'https://raw.githubusercontent.com/VandyckLN/js-developer-portfolio/main/data/profile.json';
+const url = 'https://raw.githubusercontent.com/VandyckLN/js-developer-portfolio/blob/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
 }
+
